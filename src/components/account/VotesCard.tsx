831ee1912a537353fbcea1bf287d59983bce2a58
phantom930 <phantom93930@gmail.com>
2022-07-30 18:27:27 +0900

    fix:vote-history-account-detail

--- src/components/account/VotesCard.tsx
+++ src/components/account/VotesCard.tsx
@@ -1,53 +1,41 @@
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import ContentCard from "components/common/ContentCard";
 import { Slot } from "components/common/Slot";
-import React from "react";
 import { VoteAccount, Vote } from "validators/accounts/vote";
 
 export function VotesCard({ voteAccount }: { voteAccount: VoteAccount }) {
   return (
-    <>
-      <div className="card">
-        <div className="card-header">
-          <div className="row align-items-center">
-            <div className="col">
-              <h3 className="card-header-title">Vote History</h3>
-            </div>
-          </div>
-        </div>
-
-        <div className="table-responsive mb-0">
-          <table className="table table-sm table-nowrap card-table">
-            <thead>
-              <tr>
-                <th className="w-1 text-muted">Slot</th>
-                <th className="text-muted">Confirmation Count</th>
-              </tr>
-            </thead>
-            <tbody className="list">
-              {voteAccount.info.votes.length > 0 &&
-                voteAccount.info.votes
-                  .reverse()
-                  .map((vote: Vote, index) => renderAccountRow(vote, index))}
-            </tbody>
-          </table>
-        </div>
-
-        <div className="card-footer">
-          <div className="text-muted text-center">
-            {voteAccount.info.votes.length > 0 ? "" : "No votes found"}
-          </div>
-        </div>
-      </div>
-    </>
+    <ContentCard
+      title={<Typography variant="h4">Vote History</Typography>}
+      footer={voteAccount.info.votes.length > 0 ? "" : "No votes found"}
+    >
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Slot</TableCell>
+              <TableCell align="right">Confirmation Count</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {voteAccount.info.votes.length > 0 &&
+              voteAccount.info.votes
+                .reverse()
+                .map((vote: Vote, index) => renderAccountRow(vote, index))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </ContentCard>
   );
 }
 
 const renderAccountRow = (vote: Vote, index: number) => {
   return (
-    <tr key={index}>
-      <td className="w-1 text-monospace">
+    <TableRow key={index}>
+      <TableCell className="w-1 text-monospace">
         <Slot slot={vote.slot} link />
-      </td>
-      <td className="text-monospace">{vote.confirmationCount}</td>
-    </tr>
+      </TableCell>
+      <TableCell align="right">{vote.confirmationCount}</TableCell>
+    </TableRow>
   );
 };
