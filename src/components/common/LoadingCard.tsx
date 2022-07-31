fc8916fb9f4f8384463fd42b831238b55b426670
phantom930 <phantom93930@gmail.com>
2022-08-01 05:08:43 +0900

    fix:loading-add-icon

--- src/components/common/LoadingCard.tsx
+++ src/components/common/LoadingCard.tsx
@@ -1,9 +1,13 @@
+import { CircularProgress } from "@mui/material";
 import ContentCard from "./ContentCard";
 
 export function LoadingCard({ message, className }: { message?: string, className?: string }) {
   return (
     <ContentCard className={`p-4 text-center ${className}`}>
-      {message || "Loading"}
+      <div className="flex justify-center items-center gap-2">
+        <CircularProgress size={20} color="primary" />
+        {message || "Loading"}
+      </div>
     </ContentCard>
   );
 }
